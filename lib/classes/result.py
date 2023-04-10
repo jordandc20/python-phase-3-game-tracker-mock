0a480b0db3cbfaf145898a4a59a90dc74dcdd18d
Diana Jordan <dianacjordan@gmail.com>
2023-04-10 16:14:46 -0400

    practice

--- lib/classes/result.py
+++ lib/classes/result.py
@@ -1,3 +1,31 @@
 class Result:
-    def __init__(self):
-        pass
+    
+    all= []
+
+    def __init__(self, player, game, score):
+        if 1 <= score <= 5000:
+            self.player = player
+            self.game = game
+            self.score = score
+            Result.all.append(self)
+            
+    @classmethod
+    def get_all(cls):
+        return cls.all
+            
+    
+
+
+# Result property player
+    # Returns the player for the Result
+    # Players must be Player instances
+# Result property game
+    # Returns the game that was played
+    # Games must be Game instances
+
+
+# Result __init__(self, player, game, score)
+# Result is initialized with a Player instance, a Game instance, and a score (number)
+# Result property score
+# Returns the score for the Result instance
+# Scores must be integers between 1 and 5000, inclusive
